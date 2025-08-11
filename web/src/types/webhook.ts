a8d3c78eb52c83209810851262206589484d9dba
Corbin <corbin.cargil@gmail.com>
2025-08-11 11:00:02 -0500

    add react-hook-form, zod

--- web/src/types/webhook.ts
+++ web/src/types/webhook.ts
@@ -1,7 +1,9 @@
+import { z } from "zod";
+
 export interface Webhook {
   uuid: string;
   name: string;
-  description: string;
+  description?: string;
   slug: string;
   notificationTitle: string;
   notificationMessage: string;
@@ -10,3 +12,14 @@ export interface Webhook {
   createdAt: string;
   updatedAt: string;
 }
+
+export const createWebhookSchema = z.object({
+  name: z.string().min(1),
+  description: z.string().optional(),
+  slug: z.string().min(1),
+  notificationTitle: z.string().min(1),
+  notificationMessage: z.string().min(1),
+  isActive: z.boolean(),
+});
+
+export type CreateWebhook = z.infer<typeof createWebhookSchema>;
